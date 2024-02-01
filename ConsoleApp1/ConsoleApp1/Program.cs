e5f35e29d7cfd8dd10e493682ee1ca8916aa22a0
VoinarovytchVadym <ipz224_vvv@student.ztu.edu.ua>
2024-02-01 16:10:08 +0200

    Add code

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+System.Globalization.CultureInfo customCulture =
+(System.Globalization.CultureInfo)
+ System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+customCulture.NumberFormat.NumberDecimalSeparator = ".";
+System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+Console.OutputEncoding = Encoding.Unicode;
+Console.InputEncoding = Encoding.Unicode;
+
+Console.WriteLine("Введіть перше число");
+double x = double.Parse(Console.ReadLine());
+Console.WriteLine("Введіть друге число");
+double y = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"{x}+{y}= {x+y}");
